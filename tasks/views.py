c286b0648d7f55cbd54c085997a0abb6469f78e5
RemonAnsary <rasel15-10514@diu.edu.bd>
2025-01-26 18:22:09 +0600

    views fixed

--- tasks/views.py
+++ tasks/views.py
@@ -6,12 +6,14 @@ from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['status']  # Enables filtering by 'status' using ?status=...
-    search_fields = ['title', 'description']  # Enables searching using ?search=...
+    filterset_fields = ['status']  
+    search_fields = ['title', 'description']  
     
     @action(detail=False, methods=['get'])
     def overdue(self, request):
