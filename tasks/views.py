a844ae7fa57600cf781cc95266b6dd9da1cade4e
RemonAnsary <rasel15-10514@diu.edu.bd>
2025-01-26 18:20:57 +0600

    api view fixed

--- tasks/views.py
+++ tasks/views.py
@@ -5,7 +5,7 @@ from .serializers import TaskSerializer
 from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
-
+from rest_framework.response import Response
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
